50098a483d3cbdbb87aab79fa6cbb80f595b3fdf
Rj <1rjsingh1@gmail.com>
2021-09-23 16:46:56 +0530

    Toggle

--- src/app/components/expense-modal/expense-modal.component.ts
+++ src/app/components/expense-modal/expense-modal.component.ts
@@ -8,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class ExpenseModalComponent implements OnInit {
 
   constructor() { }
-
+  showRecur:boolean=false;
   ngOnInit(): void {
+  }
+  onChange(evt)
+  {
+    if(evt.target.checked)
+      this.showRecur=true;
+      else
+      this.showRecur=false;
   }
 
 }
