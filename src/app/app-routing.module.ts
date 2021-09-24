81153f0e33ef24b8f88bdbf2096dc8f07f035874
Ritika Goyal <54425330+Ritika372@users.noreply.github.com>
2021-09-24 15:08:44 +0530

    login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddIncomeComponent } from './components/add-income/add-income.component';
 import { ExpenseModalComponent } from './components/expense-modal/expense-modal.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: 'home', component: HomepageComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'addexpense', component: ExpenseModalComponent }
+  { path: 'addexpense', component: ExpenseModalComponent },
+  {path: "login" , component: LoginComponent}
 ];
 
 @NgModule({
