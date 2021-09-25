099075d77fc7fbaebe6d66f647fa54e4bd783d0d
Rj <1rjsingh1@gmail.com>
2021-09-25 20:58:44 +0530

    Google Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,33 @@ import { ExpenseModalComponent } from './components/expense-modal/expense-modal.
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import {SocialAuthServiceConfig} from 'angularx-social-login';
+import {SocialLoginModule,GoogleLoginProvider} from 'angularx-social-login';
+
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, HomepageComponent, AddIncomeComponent,ExpenseModalComponent, LoginComponent, RegisterComponent ],
-  imports: [HttpClientModule, BrowserModule, AppRoutingModule, FormsModule],
-  providers: [],
+  imports: [HttpClientModule, BrowserModule, AppRoutingModule, FormsModule,
+    SocialLoginModule],
+  providers: [
+    {
+    provide:'SocialAuthServiceConfig',
+    useValue:{
+      autoLogin: false,
+      providers:[
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider('1095845763452-cn4jebbhe2ge08jtm36fgp1pld2u2m2n.apps.googleusercontent.com')
+        }
+      ],
+      onError: (err) => {
+        console.error(err);
+      }
+    }  as SocialAuthServiceConfig
+  }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {SocialAuthService} from 'angularx-social-login';
+import {SocialUser,GoogleLoginProvider} from 'angularx-social-login';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
   ],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  user:SocialUser;
+  constructor(private authService: SocialAuthService) {}
+
+  ngOnInit(): void {
+    this.authService.authState.subscribe((user)=>
+    {
+      this.user=user;
+      console.log(this.user.email);
+      console.log(this.user);
+    });
+  }
+
+  signInWithGoogle() : any
+  {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+   
+  }
+
+
 }
